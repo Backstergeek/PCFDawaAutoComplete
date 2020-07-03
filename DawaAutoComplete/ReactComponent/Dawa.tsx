f8d58c839875abe6c7fad928e2251e05929e7bcf
Andreas H B <ahb@jdm.dk>
2020-07-03 13:37:36 +0200

    Refactor

--- DawaAutoComplete/ReactComponent/Dawa.tsx
+++ DawaAutoComplete/ReactComponent/Dawa.tsx
@@ -5,9 +5,11 @@ import { Idawa, IdawaProps } from '../Interfaces/interface';
 export const Dawa: React.FC<IdawaProps> = (props) => {
 
     const [add, setAdd] = React.useState<Idawa[]>([])
+    const [temp, setTemp] = React.useState<string>("");
 
     const GetDawa = async (input: string) => {
         fetch(`https://dawa.aws.dk/adgangsadresser/autocomplete?q=${input}`).then(res => res.json()).then((r: Idawa[]) => setAdd(r))
+        setTemp(input)
     }
 
     return (
@@ -15,16 +17,19 @@ export const Dawa: React.FC<IdawaProps> = (props) => {
             <TextField placeholder="Indtast en addresse"
             onChange={async (event, value) =>{
                 await GetDawa((value !== undefined) ? value : "")
-            }} />
+            }} 
+            value={temp}
+            />
             <ul id="DawaList" style={{listStyle: "none"}}>
                 {add.map(a => (
                     <li onClick={() => {
                         if(props.changeAddress)
                         props.changeAddress(a.adgangsadresse.vejnavn, a.adgangsadresse.postnr, a.adgangsadresse.postnrnavn,a.adgangsadresse.husnr)
+                        setTemp("");
+                        setAdd([]);
                     }} key={a.adgangsadresse.id}>{a.tekst}</li>
                 ))}
             </ul>
         </div>
-        
     );
 }
